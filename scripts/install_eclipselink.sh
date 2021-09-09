0553ac4dcd3dac45e2ea9f9e66c8c19a412442a8
DANPINT <d.pinto@kigroup.de>
2021-09-09 15:47:47 +0100

    fix install_eclipselink.sh

--- scripts/install_eclipselink.sh
+++ scripts/install_eclipselink.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+ECLIPSELINK_PATH=$WILDFLY_HOME/modules/system/layers/base/org/eclipse/persistence/main
+
 echo "=> Downloading EclipseLink v$ECLIPSELINK_VERSION"
-curl https://ftp.acc.umu.se/mirror/eclipse.org/rt/eclipselink/releases/$ECLIPSELINK_VERSION/eclipselink.jar --output /tmp/eclipselink.jar
+curl https://ftp.acc.umu.se/mirror/eclipse.org/rt/eclipselink/releases/$ECLIPSELINK_VERSION/eclipselink.jar --output $ECLIPSELINK_PATH/eclipselink.jar
 
 echo "=> Installing EclipseLink v$ECLIPSELINK_VERSION"
-ECLIPSELINK_PATH=$WILDFLY_HOME/modules/system/layers/base/org/eclipse/persistence/main
 sed -i "s/<\/resources>/\n\
-        <resource-root path=\"eclipselink-${ECLIPSELINK_VERSION}.jar\">\n \
+        <resource-root path=\"eclipselink.jar\">\n \
             <filter>\n \
                 <exclude path=\"javax\/**\" \/>\n \
             <\/filter>\n \
